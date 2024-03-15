c16cfbeb697d527683094df06dca050b11d03132
Shehzain Hassan <l200937@lhr.nu.edu.pk>
2024-03-15 18:06:19 +0500

    Added Sign Up

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import UserLogin from "./pages/UserLogin";
+import SignUp from "./pages/UserSignUp";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<UserLogin />} />
+        <Route path="/signUp" element={<SignUp />} />
       </Routes>
     </Router>
   );
