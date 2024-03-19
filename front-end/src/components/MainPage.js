004dbc3163d827aa81daab8c9695a3dbf14a1b2c
Ahmad Mamoon <ahmad.mamoon50@gmail.com>
2024-03-20 02:38:21 +0500

    Updated Frontend
    Added Navbar and Sidebar
    Modified the Admin and User login/signup

---
+++ front-end/src/components/MainPage.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Sidebar from "./Sidebar";
+import Navbar from "./Navbar";
+
+function MainPage() {
+  return (
+    <>
+    <Navbar />
+    <div className="flex items-center flex-row">
+      <div>
+        <Sidebar />
+      </div>
+    </div>
+    </>
+  );
+}
+
+export default MainPage;
